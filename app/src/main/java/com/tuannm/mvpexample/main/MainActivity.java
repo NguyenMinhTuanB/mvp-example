32df44a8de55d6d574c04c0c3bf807f0e9916313
tuannguyen38 <tuannm.it.hust@gmail.com>
2018-10-28 16:55:25 +0700

    finish code

--- app/src/main/java/com/tuannm/mvpexample/main/MainActivity.java
+++ app/src/main/java/com/tuannm/mvpexample/main/MainActivity.java
@@ -1,7 +1,9 @@
-package com.tuannm.mvpexample;
+package com.tuannm.mvpexample.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.tuannm.mvpexample.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/tuannm/mvpexample/signup/SignUpActivity.java
@@ -0,0 +1,19 @@
+package com.tuannm.mvpexample.signup;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.tuannm.mvpexample.R;
+
+/**
+ * Created by TuanNM on 28/10/2018.
+ * descripsiont:
+ */
+public class SignUpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
